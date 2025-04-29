277464277c1428a39151bc58b97941a958ee2444
RafaelSartoretto <161588088+RafaelSartoretto@users.noreply.github.com>
2025-04-29 12:17:32 +0000

    aiiiiiiiiii

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-    const diminuiFonteBotao = document.getElementById ('diminuir-fonte')
-    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
-const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+    const diminuiFonteBotao = document.getElementById ('diminuir-fonte');
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+    const alternaContraste = document.getElementById('alterna-contraste');
+    
 botaoDeAcessibilidade.addEventListener('click', function (){
     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
     opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
    })
 
     let tamanhoAtualFonte = 1;
+
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
@@ -20,5 +23,9 @@ botaoDeAcessibilidade.addEventListener('click', function (){
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
     });
+
+    alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste')
+})
 
 });
